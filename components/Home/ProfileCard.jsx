469e4ed1ecb7cff6126f59cb223b5602f682e9c1
Chris Hong <hongchris97@gmail.com>
2023-09-04 22:30:19 -0700

    images are displaying for cards

--- components/Home/ProfileCard.jsx
+++ components/Home/ProfileCard.jsx
@@ -11,38 +11,42 @@ const ProfileCard = () => {
             const updatedUsers = []
 
             for (const user of seed) {
-                console.log('user user user', user)
+                // console.log('user user user', user)
                 try {
                     const response = await fetch(user.fetch)
                     if (response.ok) {
                         const data = await response.json()
+                        const updatedUser = { ...user, randomImage: data.message };
+
                         // console.log('data', data)
-                        user.randomImage = data.message; 
+                        // user.randomImage = data.message; 
 
-
+                        updatedUsers.push(updatedUser)
+                        setUsers(updatedUsers)
                     } else {
                         console.error('Failed to fetch image')
                     }
                 } catch (error) {
                     console.error('Error fetching image:', error.message)
                 }
-                updatedUsers.push(user)
-                setUsers(updatedUsers)
+                // updatedUsers.push(user)
+                // setUsers(updatedUsers)
             }
         }
 
         fetchImagesForUsers()
     }, [])
 console.log('updated user:', users)
+// console.log('user.randomImage:', users[0].randomImage);
+
 
     return (
         <View>
             {users.map((user) => (
                 <View key={user.id}>
                     <Text>{user.username}</Text>
-                    <Text>{user.randomImage}</Text>
                     {user.randomImage && (
-                        <Image source={{ uri: user.randomImage}} />
+                        <Image source={{uri: user.randomImage}} style={{ resizeMode: 'cover', width: '75%', height: '75%' }}/> 
                     )}
                     {/* <Image src={user.randomImage} style={{height:'200px', width:'200px'}}/> */}
                 </View>
