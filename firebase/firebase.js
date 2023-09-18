8833cef180419ff0178ac661a47be2517712d879
Chris Hong <hongchris97@gmail.com>
2023-09-17 20:51:38 -0700

    firebase config

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,17 +1,22 @@
 import * as firebase from 'firebase/app'
+import 'firebase/firestore';
 import 'firebase/auth'
 import 'firebase/database'
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "YOUR_DATABASE_URL",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID",
+    apiKey: process.env.YOUR_API_KEY,
+    authDomain: process.env.YOUR_AUTH_DOMAIN,
+    databaseURL: process.env.YOUR_DATABASE_URL,
+    projectId: process.env.YOUR_PROJECT_ID,
+    storageBucket: process.env.YOUR_STORAGE_BUCKET,
+    messagingSenderId: process.env.YOUR_MESSAGING_SENDER_ID,
+    appId: process.env.YOUR_APP_ID,
 }
 
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 
-export default firebase;
+const firestore = firebase.firestore();
+
+export default firestore;
